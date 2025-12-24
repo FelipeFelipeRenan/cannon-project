fc1fb9008bb7f664605df632d3977def5297e826
Felipe Renan <49200668+FelipeFelipeRenan@users.noreply.github.com>
2025-12-24 12:18:25 -0300

    feat(core ): use mpsc channel for shot results
    - Refactor request handling to use mpsc channel for results.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::{sync::Arc, time::Instant};
 
 use clap::Parser;
 use reqwest;
-use tokio::task;
+use tokio::{sync::mpsc, task};
 
 #[derive(Parser, Debug)]
 #[command(
@@ -18,6 +18,11 @@ struct Args {
     count: u32,
 }
 
+struct ShotResult {
+    success: bool,
+    duration: std::time::Duration,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
@@ -25,48 +30,60 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = Arc::new(reqwest::Client::new());
     let url = Arc::new(args.url);
 
+    let (tx, mut rx) = mpsc::channel(args.count as usize);
+
     println!("🎯 Alvo: {}", url);
     println!("🚀 Preparando o canhão para {} disparo(s)...", args.count);
 
     let start_test = Instant::now();
 
-    let mut handles = vec![];
-
-    for i in 1..=args.count {
+    for _ in 0..args.count {
         let client_clone = Arc::clone(&client);
 
         let url_clone = Arc::clone(&url);
 
-        let handle = task::spawn(async move {
+        let tx_clone = tx.clone();
+
+        task::spawn(async move {
             let start_request = Instant::now();
 
             let response = client_clone.get(url_clone.as_str()).send().await;
 
-            match response {
-                Ok(res) => {
-                    let duration = start_request.elapsed();
-                    println!(
-                        "Tiro #{}: Status {} - Tempo: {:?}",
-                        i,
-                        res.status(),
-                        duration
-                    );
-                }
-                Err(e) => {
-                    println!("Tiro #{}: ❌ FALHA - Erro: {}", i, e);
-                }
-            }
+            let success = response.is_ok() && response.unwrap().status().is_success();
+
+            let _ = tx_clone
+                .send(ShotResult {
+                    success,
+                    duration: start_request.elapsed(),
+                })
+                .await;
         });
-
-        handles.push(handle);
     }
 
-    for handle in handles{
-        let _ = handle.await;
+    drop(tx);
+
+    let mut success_count = 0;
+    let mut failure_count = 0;
+    let mut total_latency = std::time::Duration::new(0, 0);
+
+    while let Some(result) = rx.recv().await {
+        if result.success {
+            success_count += 1;
+            total_latency += result.duration;
+        } else {
+            failure_count += 1;
+        }
     }
+
+    let duration = start_test.elapsed();
+    println!("\n--- 🏁 RELATÓRIO DO CANNON ---");
+    println!("Tempo total: {:?}", duration);
+    println!("Sucessos:    {}", success_count);
+    println!("Falhas:      {}", failure_count);
     
-    let total_duration = start_test.elapsed();
-    println!("\n🏁 Teste finalizado em {:?}", total_duration);
+    if success_count > 0{
+        println!("Média:   {:?}", total_latency / success_count);
+    }
 
     Ok(())
 }
